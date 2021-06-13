44fd9ed3bf07fada5d5326a72fc37d862f62cd82
andyzxwang <andyzxwang@tencent.com>
2021-06-13 20:49:02 +0800

    feat(工程): hello world

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  // parser: 'typescript-eslint-parser',
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+        extensions: ['.ts'],
+      },
+    },
+  },
+};
